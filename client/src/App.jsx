74822204fec4b7b64dce1c2559ad5641f4f94cfd
abduhamidbotirov <abduhamidbotirovwork@gmail.com>
2024-06-11 10:30:54 +0500

    finished Header full media

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Router from './routes';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const App = () => {
-
-
   return (
     <div>
-    <Router/>
-  
+      <Router />
+
     </div>
   )
 }
--- client/src/pages/news/News.jsx
+++ client/src/pages/news/News.jsx
@@ -15,50 +15,50 @@ const News = () => {
     <div className='page news-page'>
       <section className='section_one'>
         <div className='overlay'>
-            <div className='container'>
-                <div className='left'>
-                    <h2>YANGILIKLAR</h2>
-                </div>
-                <div className='right'>
-                    <Links/>
-                </div>
+          <div className='container'>
+            <div className='left'>
+              <h2>YANGILIKLAR</h2>
             </div>
+            <div className='right'>
+              <Links />
+            </div>
+          </div>
         </div>
       </section>
       <section className='section_two'>
         <div className='container'>
-            <h3 className='title'>NASHRLAR SONI: 1694</h3>
-            <div className='news_wrp'>
-                <NewsCard/>
-                <NewsCard/>
-                <NewsCard/>
-                <NewsCard/>
-                <NewsCard/>
-                <NewsCard/>
-                <NewsCard/>
-                <NewsCard/>
-                <NewsCard/>
-                <NewsCard/>
-        </div>
-        <div className='pagination_btns'>
-                <div className='left_btns'>
-                      <button className='prev'>
-                        <FaChevronLeft/> Пред. стр
-                      </button>
-                      <button className='next'>След. стр <FaChevronRight/> </button>
-                </div>
-                <div className='right_btns'>
-                    <div className='left_page'>
-                        <p>страница</p>
-                        <button>3</button>
-                        <p>из 157</p>
-                    </div>
-                    <button className='go_'>Перейти <FaChevronRight/></button>
-                </div>
+          <h3 className='title'>NASHRLAR SONI: 1694</h3>
+          <div className='news_wrp'>
+            <NewsCard />
+            <NewsCard />
+            <NewsCard />
+            <NewsCard />
+            <NewsCard />
+            <NewsCard />
+            <NewsCard />
+            <NewsCard />
+            <NewsCard />
+            <NewsCard />
+          </div>
+          <div className='pagination_btns'>
+            <div className='left_btns'>
+              <button className='prev'>
+                <FaChevronLeft /> Пред. стр
+              </button>
+              <button className='next'>След. стр <FaChevronRight /> </button>
             </div>
-            <div className="back_navigate">
-            <button className="home"><FaHome/> Bosh sahifa</button>
-            <button className="next"> <FaChevronRight/> </button>
+            <div className='right_btns'>
+              <div className='left_page'>
+                <p>страница</p>
+                <button>3</button>
+                <p>из 157</p>
+              </div>
+              <button className='go_'>Перейти <FaChevronRight /></button>
+            </div>
+          </div>
+          <div className="back_navigate">
+            <button className="home"><FaHome /> Bosh sahifa</button>
+            <button className="next"> <FaChevronRight /> </button>
             <button className="about">Normativ xujjatlar</button>
           </div>
         </div>
